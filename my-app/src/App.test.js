99d6165ce95921ea1d2d773edc81b672da8b535d
stephaniegalindo10 <stephanimatedworld@gmail.com>
2019-01-19 11:11:08 -0800

    changes card to cards

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 import React, { Component } from "react";
 
-import Card from "./components/Card";
+import Card from "./components/Cards";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
